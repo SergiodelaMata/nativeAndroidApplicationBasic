3973750681f872e80ff0b6b524d1e27623a91137
SergiodelaMata <sergio.matam@edu.uah.es>
2022-04-12 20:47:39 +0200

    :sparkles: Created structures to created dynamic tables.

---
+++ app/src/main/java/com/example/nativeandroidapplicationbasic/DynamicTable.java
@@ -0,0 +1,231 @@
+package com.example.nativeandroidapplicationbasic;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import com.example.nativeandroidapplicationbasic.db.Subject;
+
+import java.util.ArrayList;
+
+public class DynamicTable {
+    //Etiqueta logcat
+    private static final String LOG = "DynamicTable";
+    private final MainActivity mainActivity;
+    private final TableLayout tableLayout;
+    private final Context context;
+    private int[] header;
+    private ArrayList<Subject> data;
+    private TableRow tableRow;
+    private TextView textCell;
+
+    private int indexCell;
+    private int indexRow;
+
+    public DynamicTable(MainActivity mainActivity, TableLayout tableLayout, Context context) {
+        this.mainActivity = mainActivity;
+        this.tableLayout = tableLayout;
+        this.context = context;
+    }
+
+    /**
+     * Añade los textos que aparecerán en la cabecera de la tabla
+     * @param header Textos que aparecerán como cabeceras
+     */
+    public void addHeader(int[] header)
+    {
+        this.header = header;
+        createHeader();
+    }
+
+    /**
+     * Añade los datos de las asignaturas que aparecerán en la tabla
+     * @param data Datos de las distintas asignaturas que aparecerán en el cuerpo de la tabla
+     */
+    public void addData(ArrayList<Subject> data)
+    {
+        this.data = data;
+        createDataTable();
+    }
+
+    /**
+     * Crea una nueva fila de la tabla
+     */
+    public void newRow()
+    {
+        tableRow = new TableRow(context);
+        tableRow.setWeightSum(1);
+    }
+
+    /**
+     * Crea una nueva celda para la tabla
+     */
+    public void newCellHeader()
+    {
+        textCell = new TextView(context);
+        textCell.setGravity(Gravity.CENTER);
+        textCell.setTextSize(22);
+        textCell.setTextColor(Color.parseColor("#000000"));
+    }
+
+    /**
+     * Crea una nueva celda para la tabla
+     */
+    public void newCell()
+    {
+        textCell = new TextView(context);
+        textCell.setGravity(Gravity.CENTER);
+        textCell.setTextSize(22);
+        textCell.setTextColor(Color.parseColor("#000000"));
+        textCell.setWidth((int) (getScreenWidth() * 0.20));
+    }
+
+    /**
+     * Obtiene el ancho de la pantalla del dispositivo
+     * @return Valor en píxeles del ancho de la pantalla
+     */
+    public int getScreenWidth() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        mainActivity.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
+    /**
+     * Crea la cabecera de la tabla
+     */
+    public void createHeader()
+    {
+        indexCell = 0;
+        newRow();
+        while(indexCell < header.length)
+        {
+            newCellHeader();
+            textCell.setText(context.getResources().getString(header[indexCell++]));
+            tableRow.addView(textCell, newTableRowParams());
+        }
+        tableLayout.addView(tableRow);
+    }
+
+    /**
+     * Crea el cuerpo de la tabla
+     */
+    private void createDataTable()
+    {
+        String info;
+
+        for(indexRow = 0; indexRow < data.size(); indexRow++)
+        {
+            newRow();
+            for(indexCell = 0; indexCell < header.length; indexCell++)
+            {
+                newCell();
+                Subject subject = data.get(indexRow);
+                switch(indexCell)
+                {
+                    case 0:
+                        info = String.valueOf(subject.getMark());
+                        textCell.setText(info);
+                        if(subject.getMark() < 5)
+                        {
+                            textCell.setBackground(ContextCompat.getDrawable(mainActivity, R.drawable.rounded_rectangule_red));
+                        }
+                        else if(subject.getMark() >= 5 && subject.getMark() <= 8.5)
+                        {
+                            textCell.setBackground(ContextCompat.getDrawable(mainActivity, R.drawable.rounded_rectangule_yellow));
+                        }
+                        else
+                        {
+                            textCell.setBackground(ContextCompat.getDrawable(mainActivity, R.drawable.rounded_rectangule_green));
+                        }
+
+                        tableRow.addView(textCell, newTableRowParams());
+                        break;
+                    case 1:
+                        Button buttonSubject = new Button(context);
+                        buttonSubject.setId(subject.getIdSubject());
+                        buttonSubject.setText(subject.getName());
+                        buttonSubject.setBackgroundColor(Color.parseColor("#FFFFFF"));
+                        buttonSubject.setTextColor(Color.parseColor("#000000"));
+                        buttonSubject.setTextSize(16);
+                        buttonSubject.setGravity(Gravity.CENTER);
+                        buttonSubject.setWidth((int) (getScreenWidth() * 0.72));
+                        buttonSubject.setAllCaps(false);
+                        tableRow.addView(buttonSubject, newTableRowParams());
+                        break;
+                    /*case 0:
+                        info = String.valueOf(subject.getMark());
+                        textCell.setText(info);
+                        tableRow.addView(textCell, newTableRowParams());
+                        break;
+                    case 1:
+                        info = subject.getName();
+                        textCell.setText(info);
+                        tableRow.addView(textCell, newTableRowParams());
+                        break;
+                    case 2:
+                        info = "Hola";
+                        textCell.setText(info);
+                        tableRow.addView(textCell, newTableRowParams());
+                        break;*/
+                }
+            }
+            tableRow.setGravity(Gravity.CENTER_VERTICAL);
+            tableLayout.addView(tableRow);
+        }
+    }
+
+    /**
+     * Establece los márgenes de las filas de la tabla
+     * @return Ajustes para los márgenes de las filas de la tabla
+     */
+    private TableRow.LayoutParams newTableRowParams()
+    {
+        TableRow.LayoutParams params = new TableRow.LayoutParams();
+        params.setMargins(1,1,1,1);
+        params.weight = 1;
+        return params;
+    }
+
+    /**
+     * Realiza la limpieza de todos campos que había en la tabla
+     */
+    public void resetTable()
+    {
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                tableRow.removeAllViews();
+                tableLayout.removeAllViewsInLayout();
+            }
+        });
+    }
+
+    public void addHeader()
+    {
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                addHeader(Singleton.getInstance().getHeaderTableTextSubjects());
+            }
+        });
+    }
+
+    public void addData()
+    {
+        mainActivity.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                data = new ArrayList<>();
+                mainActivity.setRows(new ArrayList<>());
+                addData(mainActivity.getData());
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nativeandroidapplicationbasic/Singleton.java
@@ -0,0 +1,46 @@
+package com.example.nativeandroidapplicationbasic;
+
+import com.example.nativeandroidapplicationbasic.db.Subject;
+
+import java.util.ArrayList;
+
+public class Singleton {
+    private static Singleton instance;
+    //private int[] headerTableTextSubjects = {R.string.Mark, R.string.Subject, R.string.Actions};
+    private int[] headerTableTextSubjects = {R.string.Mark, R.string.Subject};
+    private static ArrayList<Subject> listSubjects;
+
+    public static synchronized Singleton getInstance()
+    {
+        if(instance == null)
+        {
+            listSubjects = new ArrayList<>();
+            instance = new Singleton();
+            Subject subject1 = new Subject(1, 8.5, "Metodologías Ágiles", "2021-03-15");
+            listSubjects.add(subject1);
+            Subject subject2 = new Subject(2, 9, "Cloud Computing y Contenedores", "2021-03-13");
+            listSubjects.add(subject2);
+            Subject subject3 = new Subject(3, 9.4, "Integración Continua en el Desarrollo Ágil", "2021-03-12");
+            listSubjects.add(subject3);
+            Subject subject4 = new Subject(4, 9.2, "Frameworks Backend", "2021-03-10");
+            listSubjects.add(subject4);
+        }
+        return instance;
+    }
+
+    public int[] getHeaderTableTextSubjects() {
+        return headerTableTextSubjects;
+    }
+
+    public void setHeaderTableTextSubjects(int[] headerTableTextSubjects) {
+        this.headerTableTextSubjects = headerTableTextSubjects;
+    }
+
+    public static ArrayList<Subject> getListSubjects() {
+        return listSubjects;
+    }
+
+    public static void setListSubjects(ArrayList<Subject> listSubjects) {
+        Singleton.listSubjects = listSubjects;
+    }
+}
