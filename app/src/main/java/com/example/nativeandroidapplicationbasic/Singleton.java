478acbc0c04c9646f393c71381dca389a20c53a7
SergiodelaMata <sergio.matam@edu.uah.es>
2022-04-15 12:22:16 +0200

    :sparkles: Update the format for the dates established for the last update of the mark.

--- app/src/main/java/com/example/nativeandroidapplicationbasic/Singleton.java
+++ app/src/main/java/com/example/nativeandroidapplicationbasic/Singleton.java
@@ -16,13 +16,13 @@ public class Singleton {
         {
             listSubjects = new ArrayList<>();
             instance = new Singleton();
-            Subject subject1 = new Subject(1, 8.5, "Metodologías Ágiles", "2021-03-15");
+            Subject subject1 = new Subject(1, 8.5, "Metodologías Ágiles", "2021-03-15 12:00");
             listSubjects.add(subject1);
-            Subject subject2 = new Subject(2, 9, "Cloud Computing y Contenedores", "2021-03-13");
+            Subject subject2 = new Subject(2, 9, "Cloud Computing y Contenedores", "2021-03-13 8:00");
             listSubjects.add(subject2);
-            Subject subject3 = new Subject(3, 9.4, "Integración Continua en el Desarrollo Ágil", "2021-03-12");
+            Subject subject3 = new Subject(3, 9.4, "Integración Continua en el Desarrollo Ágil", "2021-03-12 10:30");
             listSubjects.add(subject3);
-            Subject subject4 = new Subject(4, 9.2, "Frameworks Backend", "2021-03-10");
+            Subject subject4 = new Subject(4, 9.2, "Frameworks Backend", "2021-03-10 18:00");
             listSubjects.add(subject4);
         }
         return instance;
